351c7a280c899e459198073f3d9fb8be5982c6d5
박진성 <plashdof@gmail.com>
2022-09-04 02:24:05 +0900

    Fragment 추가

--- app/src/main/java/com/shoppi/app/SplashActivity.kt
+++ app/src/main/java/com/shoppi/app/SplashActivity.kt
@@ -5,11 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SplashActivity: AppCompatActivity(){
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this,MainActivity::class.java))
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
 }
