6fce432a2bdcfb925d9ba3736b334ad152941483
박진성 <plashdof@gmail.com>
2022-09-05 17:30:52 +0900

    homegragment toolbar 디자인, gradle사용 (실패)

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.shoppi.app
 
+import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.json.JSONObject
+import org.w3c.dom.Text
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -29,14 +35,26 @@ class HomeFragment : Fragment() {
         val assetLoader = AssetLoader()
         val homeData = assetLoader.getJsonString(requireContext(), "home.json")
 
+        var iconUrl = ""
+        var text = ""
+
         if(!homeData.isNullOrEmpty()){
             val jsonObject = JSONObject(homeData)
             val title = jsonObject.getJSONObject("title")
-            val text = title.getString("text")
-            val iconUrl = title.getString("icon_url")
+            text = title.getString("text")
+            iconUrl = title.getString("icon_url")
             val titleValue = Title(text, iconUrl)
-
         }
 
+        val textview = getView()?.findViewById<TextView>(R.id.home_toolbar_text)
+        textview?.setText(text)
+
+        val imgview = getView()?.findViewById<ImageView>(R.id.home_toolbar_image)
+
+        Glide.with(this).load(iconUrl).into(imgview)
+
     }
+
+
+
 }
